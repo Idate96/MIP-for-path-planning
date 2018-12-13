9e8d945d3c8d7dac11e9bdd708ee7b7a65d84e35
rano <rano.veder@gmail.com>
2018-12-13 16:51:21 +0100

    Added vehicle dynamics class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from gurobipy import *
+from aircraft import Vehicle
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-
-
+def entry():
+    pass
 
 if __name__ == '__main__':
     entry()
---
+++ tests/test_vehicle.py
@@ -0,0 +1,10 @@
+from src.aircraft import Vehicle
+import numpy as np
+import unittest
+
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_1(self):
+        pass
